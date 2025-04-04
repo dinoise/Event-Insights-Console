2d1ebc14e9b4bc109ac262a2dfbbc04450c7ac04
dinoise <dinoise0@pm.me>
2025-04-03 18:12:35 -0600

    typing the output of create mapping

--- app/services/event_mapping_service.py
+++ app/services/event_mapping_service.py
@@ -18,7 +18,7 @@ class EventMappingService:
     
     @staticmethod
     def create_mapping(event_type_id: int, source_id: int, event_mapping_description: str, 
-                       event_mapping_version: float, event_mapping_target_dataset: str, event_mapping_target_table: str):
+                       event_mapping_version: float, event_mapping_target_dataset: str, event_mapping_target_table: str) -> int:
         new_mapping = EventMapping(
             event_type_id=event_type_id,
             source_id=source_id,
