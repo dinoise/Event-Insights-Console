432691e9dbe11e7dd875e58e051e118173018009
dinoise <dinoise0@pm.me>
2025-04-04 20:07:19 -0600

    adding get event mapping by id

--- app/services/event_mapping_service.py
+++ app/services/event_mapping_service.py
@@ -17,6 +17,12 @@ class EventMappingService:
         return EventMappingSchema(many=True).dump(mappings)
     
     @staticmethod
+    def get_all_event_mapping_by_id(mapping_id) -> List[Dict[str, Any]]:
+        mapping = EventMapping.query.get(mapping_id)
+
+        return EventMappingSchema(many=False).dump(mapping)
+    
+    @staticmethod
     def create_mapping(
         event_type_id: int, 
         source_id: int, 
