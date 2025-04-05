0879bb9d9a6668c3ef00cece79f6a9580d5195b6
dinoise <dinoise0@pm.me>
2025-04-04 19:54:02 -0600

    list of mapping events rendered

--- app/routes/web_routes.py
+++ app/routes/web_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
 
+from services.event_mapping_service import EventMappingService
+
 bp = Blueprint('web', __name__)
 
-@bp.route('/')
-def index_route():
-    return render_template('index.html')
+@bp.route('/event-mappings')
+def show_event_mappings():
+    mappings = EventMappingService.get_all_event_mappings()        
+    return render_template('index.html', mappings=mappings)
