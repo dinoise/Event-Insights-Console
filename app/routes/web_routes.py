111de4ed2ad1eddef2e6a922be9a85a768a146bd
dinoise <dinoise0@pm.me>
2025-04-04 20:07:57 -0600

    detailed mapping view

--- app/routes/web_routes.py
+++ app/routes/web_routes.py
@@ -8,3 +8,13 @@ bp = Blueprint('web', __name__)
 def show_event_mappings():
     mappings = EventMappingService.get_all_event_mappings()        
     return render_template('index.html', mappings=mappings)
+
+@bp.route('/event-mappings/<int:mapping_id>')
+def show_event_mapping_by_id(mapping_id):
+    try:
+        mapping = EventMappingService.get_all_event_mapping_by_id(mapping_id)
+    except Exception as e:
+        print(e)
+    print(mapping)     
+
+    return render_template('mapping.html', mapping=mapping)
