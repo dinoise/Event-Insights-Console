4c350c0f1c6169b762c82e659d97245330ffae2d
dinoise <dinoise0@pm.me>
2025-04-07 14:37:37 -0600

    getting a source by primary key

--- app/services/source_service.py
+++ app/services/source_service.py
@@ -13,3 +13,15 @@ class SourceService:
     def get_all_sources() -> List[Dict[str, Any]]:
         sources = Source.query.all()
         return SourceSchema(many=True).dump(sources)
+    
+    @staticmethod
+    def get_source_by_pk(source_id: int) -> Dict[str, Any]:
+        source = Source.query.filter_by(
+            source_id=source_id,
+            source_status="ACTIVE"
+        ).first()
+        
+        if not source:
+            return None
+        
+        return SourceSchema(many=False).dump(source)
