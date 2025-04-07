5e0608488c468dc9163d3222b11875b3c8a4de79
dinoise <dinoise0@pm.me>
2025-04-07 14:41:38 -0600

    getting a event types by primary key and filtering by status when getting all events

--- app/services/event_type_service.py
+++ app/services/event_type_service.py
@@ -11,5 +11,23 @@ class EventTypeService:
 
     @staticmethod
     def get_all_event_types() -> List[Dict[str, Any]]:
-        event_types = EventType.query.all()
+        event_types = EventType.query.filter_by(
+            event_type_status="ACTIVE"
+        ).all()
+
+        if not event_types:
+            return None
+        
         return EventTypeSchema(many=True).dump(event_types)
+    
+    @staticmethod
+    def get_event_type_by_pk(event_type_id: int) -> Dict[str, Any]:
+        event_type = EventType.query.filter_by(
+            event_type_id=event_type_id,
+            event_type_status="ACTIVE"
+        ).first()
+
+        if not event_type:
+            return None
+        
+        return EventTypeSchema(many=False).dump(event_type)
