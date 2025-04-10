1b422c4f8ae1264729d78f30288d6cd45e545b97
dinoise <dinoise0@pm.me>
2025-04-09 21:26:56 -0600

    srevice for source upfate

--- app/services/source_service.py
+++ app/services/source_service.py
@@ -7,6 +7,8 @@ from schemas.source_schema import SourceSchema
 # Types
 from typing import List, Dict, Any
 
+from __init__ import db
+
 class SourceService:
 
     @staticmethod
@@ -31,3 +33,23 @@ class SourceService:
             return None
         
         return SourceSchema(many=False).dump(source)
+    
+    @staticmethod
+    def update_source(source_id: int, update_data: Dict) -> Dict[str, Any]:
+        try:
+            source = Source.query.filter_by(
+                source_id=source_id,
+                source_status="ACTIVE"
+            ).first()
+            if not source:
+                return {}
+
+            for field, value in update_data.items():
+                setattr(source, field, value)
+            
+            db.session.commit()
+
+            return SourceSchema(many=False).dump(source)
+        except Exception as e:
+            db.session.rollback()
+            raise Exception(f"Database error: {str(e)}")
